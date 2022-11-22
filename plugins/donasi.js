6f23370bfd675cf70b6d2e88a8438327731856be
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-11-22 09:36:49 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Telkomsel [+62 812-7367-7810]
+│ • INDOSAT [+62 856-0946-7937]
 │ • Xl [+62 819-5929-3465]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
 │ • https://www.instagram.com/invites/contact/?i=18n050noxcaqr&utm_content=4fkjlpc
-│ • Gopay [+62 812-7367-7810]
+│ • DANA [+62 856-0946-7937]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
